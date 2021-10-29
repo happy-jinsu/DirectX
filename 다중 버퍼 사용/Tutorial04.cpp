fba4d8c6d7ac7d1e0b275ba1c74b6f39ad67d009
이수진 <68191793+happy-jinsu@users.noreply.github.com>
2021-10-29 15:26:22 +0900

    Add files via upload

---
+++ 다중 버퍼 사용/Tutorial04.cpp
@@ -0,0 +1,429 @@
+// include the basic windows header files and the Direct3D header files
+#include <windows.h>
+#include <windowsx.h>
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dx10.h>
+
+// include the Direct3D Library file
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dx11.lib")
+#pragma comment (lib, "d3dx10.lib")
+
+// define the screen resolution
+#define SCREEN_WIDTH  800
+#define SCREEN_HEIGHT 600
+
+// global declarations
+IDXGISwapChain* swapchain;             // the pointer to the swap chain interface
+ID3D11Device* dev;                     // the pointer to our Direct3D device interface
+ID3D11DeviceContext* devcon;           // the pointer to our Direct3D device context
+ID3D11RenderTargetView* backbuffer;    // the pointer to our back buffer
+ID3D11InputLayout* pLayout;            // the pointer to the input layout
+ID3D11VertexShader* pVS;               // the pointer to the vertex shader
+ID3D11PixelShader* pPS;                // the pointer to the pixel shader
+ID3D11Buffer* pVBuffer;                // the pointer to the vertex buffer
+////////////////////1. 버퍼를 하나 더 쓰겠다고 선언./////////////////////////////////////
+ID3D11Buffer* pVBuffer2;                // the pointer to the vertex buffer 
+
+
+
+// a struct to define a single vertex
+struct VERTEX { FLOAT X, Y, Z; D3DXCOLOR Color; };
+
+// function prototypes
+void InitD3D(HWND hWnd);    // sets up and initializes Direct3D
+void RenderFrame(void);     // renders a single frame
+void CleanD3D(void);        // closes Direct3D and releases memory
+void InitGraphics(void);    // creates the shape to render    253번째 코드. 첫 버퍼
+
+//////2. 하나 더 생성. 버텍스 버퍼 추가를 위한 작업////////////////////////////////////////
+void InitGraphics2(void);    // creates the shape to render 
+
+
+void InitPipeline(void);    // loads and prepares the shaders
+
+// the WindowProc function prototype
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+
+// the entry point for any Windows program
+int WINAPI WinMain(HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow)
+{
+    HWND hWnd;
+    WNDCLASSEX wc;
+
+    ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.lpszClassName = L"WindowClass";
+
+    RegisterClassEx(&wc);
+
+    RECT wr = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+    AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+
+    hWnd = CreateWindowEx(NULL,
+        L"WindowClass",
+        L"Our First Direct3D Program",
+        WS_OVERLAPPEDWINDOW,
+        300,
+        300,
+        wr.right - wr.left,
+        wr.bottom - wr.top,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    ShowWindow(hWnd, nCmdShow);
+
+    // set up and initialize Direct3D
+    InitD3D(hWnd);
+
+    // enter the main loop:
+
+    MSG msg;
+
+    while (TRUE)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+
+            if (msg.message == WM_QUIT)
+                break;
+        }
+
+        RenderFrame();
+    }
+
+    // clean up DirectX and COM
+    CleanD3D();
+
+    return msg.wParam;
+}
+
+
+// this is the main message handler for the program
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_DESTROY:
+    {
+        PostQuitMessage(0);
+        return 0;
+    } break;
+    }
+
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+
+// this function initializes and prepares Direct3D for use
+void InitD3D(HWND hWnd)
+{
+    // create a struct to hold information about the swap chain
+    DXGI_SWAP_CHAIN_DESC scd;
+
+    // clear out the struct for use
+    ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+    // fill the swap chain description struct
+    scd.BufferCount = 1;                                   // one back buffer
+    scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;    // use 32-bit color
+    scd.BufferDesc.Width = SCREEN_WIDTH;                   // set the back buffer width
+    scd.BufferDesc.Height = SCREEN_HEIGHT;                 // set the back buffer height
+    scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;     // how swap chain is to be used
+    scd.OutputWindow = hWnd;                               // the window to be used
+    scd.SampleDesc.Count = 4;                              // how many multisamples
+    scd.Windowed = TRUE;                                   // windowed/full-screen mode
+    scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;    // allow full-screen switching
+
+    // create a device, device context and swap chain using the information in the scd struct
+    D3D11CreateDeviceAndSwapChain(NULL,
+        D3D_DRIVER_TYPE_HARDWARE,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        D3D11_SDK_VERSION,
+        &scd,
+        &swapchain,
+        &dev,
+        NULL,
+        &devcon);
+
+
+    // get the address of the back buffer
+    ID3D11Texture2D* pBackBuffer;
+    swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+    // use the back buffer address to create the render target
+    dev->CreateRenderTargetView(pBackBuffer, NULL, &backbuffer);
+    pBackBuffer->Release();
+
+    // set the render target as the back buffer
+    devcon->OMSetRenderTargets(1, &backbuffer, NULL);
+
+
+    // Set the viewport
+    D3D11_VIEWPORT viewport;
+    ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+    viewport.TopLeftX = 0;
+    viewport.TopLeftY = 0;
+    viewport.Width = SCREEN_WIDTH;
+    viewport.Height = SCREEN_HEIGHT;
+
+    devcon->RSSetViewports(1, &viewport);
+
+    InitPipeline();
+    InitGraphics();
+    InitGraphics2();////3. 추가해주기/////////////////////////////////////////////;
+
+}
+
+
+// this is the function used to render a single frame
+void RenderFrame(void)
+{
+    // clear the back buffer to a deep blue
+    devcon->ClearRenderTargetView(backbuffer, D3DXCOLOR(0.0f, 0.0f, 0.0f, 1.0f));    // 배경 색깔 바꾸는 곳
+
+    // select which vertex buffer to display
+    //두개의 버텍스 버퍼가 모두 동일한 버텍스 버퍼 스타일이므로 그대로 사용  
+    UINT stride = sizeof(VERTEX);
+    UINT offset = 0;
+    
+    //-----------------------------
+    //버텍스 버퍼1로 렌더링  
+    devcon->IASetVertexBuffers(0, 1, &pVBuffer, &stride, &offset);
+    // select which primtive type we are using
+    devcon->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    // draw the vertex buffer to the back buffer
+    devcon->Draw(26, 0);
+
+
+    //-------------------------------
+    //6. 마지막////////////////////////////////////////////////////////////////////////////////////////////////
+    //버텍스 버퍼2로 렌더링  
+    devcon->IASetVertexBuffers(0, 1, &pVBuffer2, &stride, &offset);  // 이름 바꿔주기
+    //devcon->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    //devcon->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_POINTLIST);
+    devcon->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINELIST); // 뭘 그릴건지 알려주고
+    devcon->Draw(30, 0);  //점 몇개 썼는지 교체
+
+    //-----------------------------------
+
+
+    // switch the back buffer and the front buffer
+    swapchain->Present(0, 0);  //프론트 버퍼로 옮겨서 출력
+}
+
+
+// this is the function that cleans up Direct3D and COM
+void CleanD3D(void)
+{
+    swapchain->SetFullscreenState(FALSE, NULL);    // switch to windowed mode
+
+    // close and release all existing COM objects
+    pLayout->Release();
+    pVS->Release();
+    pPS->Release();
+    pVBuffer->Release();
+    //////////////// 4. 추가로 만든 것도 릴리즈 해준다.//////////////////////////////////////////////////////
+    pVBuffer2->Release(); 
+
+    swapchain->Release();
+    backbuffer->Release();
+    dev->Release();
+    devcon->Release();
+}
+
+//버텍스 버퍼 1 만들기 
+// this is the function that creates the shape to render
+//건물 틀과 색을 입히는 버퍼
+void InitGraphics() 
+{
+    // create a triangle using the VERTEX struct
+    VERTEX OurVertices[] =
+    {
+        {-0.4f, 0.6f, 0.0f, D3DXCOLOR(0.95f, 0.158f, 0.160f, 1.0f)},
+        {0.3f, -0.9f, 0.0f, D3DXCOLOR(0.224f, 0.255f, 0.255f, 1.0f)},
+        {-0.4f, -0.9f, 0.0f, D3DXCOLOR(0.95f, 0.158f, 0.160f, 1.0f)},  // 건물 앞면 하단 삼각형 * 3
+
+        {-0.4f, 0.6f, 0.0f, D3DXCOLOR(0.95f, 0.158f, 0.160f, 1.0f)},
+        {0.3f, 0.6f, 0.0f,D3DXCOLOR(0.224f, 0.255f, 0.255f, 1.0f)},
+        {0.3f, -0.9f, 0.0f,D3DXCOLOR(0.224f, 0.255f, 0.255f, 1.0f)},   // 건물 앞면 상단 삼각형 * 6
+
+        {-0.4f, 0.6f, 0.0f,D3DXCOLOR(0.60f, 0.158f, 0.160f, 1.0f)},
+        {-0.15f, 0.7f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},      
+        {-0.15f, 0.65f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},   // 건물 윗 부분 왼쪽 벽면 상단 삼각형 * 9
+
+        {-0.4f, 0.6f, 0.0f, D3DXCOLOR(0.60f, 0.158f, 0.160f, 1.0f)},
+        {-0.15f, 0.65f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},
+        {-0.28f, 0.6f, 0.0f,D3DXCOLOR(0.60f, 0.158f, 0.160f, 1.0f)},  // 건물 윗 부분 왼쪽 벽면 하단 삼각형 * 12
+
+        {-0.15f, 0.7f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},
+        {0.55f, 0.7f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},
+        {0.425f, 0.65f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},  // 건물 윗쪽 벽면 왼->오 방향 상단 삼각형 * 15
+
+        {-0.15f, 0.7f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},
+        {0.425f, 0.65f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},
+        {-0.15f, 0.65f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},  // 건물 윗쪽 벽면 오 -> 왼 방향 하단 삼각형 * 18
+
+        {-0.15f, 0.65f, 0.0f, D3DXCOLOR(0.102f, 0.153f, 0.153f, 1.0f)},
+        {0.425f, 0.65f, 0.0f,  D3DXCOLOR(0.102f, 0.153f, 0.153f, 1.0f)},
+        {0.3f, 0.6f, 0.0f,  D3DXCOLOR(0.102f, 0.153f, 0.153f, 1.0f)},          // 옥상 바닥 상단 삼각형 * 21
+
+        {-0.15f, 0.65f, 0.0f,  D3DXCOLOR(0.102f, 0.153f, 0.153f, 1.0f)},
+        {0.3f, 0.6f, 0.0f, D3DXCOLOR(0.102f, 0.153f, 0.153f, 1.0f)},
+        {-0.28f, 0.6f, 0.0f,D3DXCOLOR(0.50f, 0.158f, 0.160f, 1.0f)},       //옥상 바닥 하단 삼각형 * 24
+
+
+        {0.3f, 0.6f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},
+        {0.55f, 0.7f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},
+        {0.55f, -0.65f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},   // 건물 옆쪽 벽면 중 상단 벽면. 윗쪽이 동일선상 변. * 26
+
+        {0.3f, 0.6f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},
+        {0.55f, -0.65f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)},
+        {0.3f, -0.9f, 0.0f, D3DXCOLOR(0.204f, 0.204f, 0.204f, 1.0f)}     // 건물 옆쪽 벽면 중 하단 벽면. 아랫쪽이 동일선상 변 * 30
+
+ 
+
+        /////////////////건물 끝//////////////
+
+
+  
+    };
+
+
+    // create the vertex buffer
+    D3D11_BUFFER_DESC bd;
+    ZeroMemory(&bd, sizeof(bd));
+
+    bd.Usage = D3D11_USAGE_DYNAMIC;                // write access access by CPU and GPU
+    bd.ByteWidth = sizeof(VERTEX) * 30;             // size is the VERTEX struct * 3
+    bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;       // use as a vertex buffer
+    bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;    // allow CPU to write in buffer
+
+    dev->CreateBuffer(&bd, NULL, &pVBuffer);       // create the buffer
+
+
+    // copy the vertices into the buffer
+    D3D11_MAPPED_SUBRESOURCE ms;
+    devcon->Map(pVBuffer, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &ms);    // map the buffer
+    memcpy(ms.pData, OurVertices, sizeof(OurVertices));                 // copy the data
+    devcon->Unmap(pVBuffer, NULL);                                      // unmap the buffer
+}
+
+//////////////////////////////////////5. 버텍스 버퍼 2 만들기////////////////////////////////////////////////// 
+// this is the function that creates the shape to render
+// 옥상의 테두리 작업
+void InitGraphics2()
+{
+    // create a triangle using the VERTEX struct
+    VERTEX OurVertices[] =
+    {
+        {-0.15f, 0.7f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+        {-0.15f, 0.65f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+
+        {-0.15f, 0.65f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+        {-0.28f, 0.6f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+
+        {-0.15f, 0.65f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+        {0.425f, 0.65f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)}, // 옥상 내부
+
+
+        {-0.15f, 0.7f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+        {-0.4f, 0.6f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+
+        {-0.15f, 0.7f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+        {0.55f, 0.7, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+
+        {-0.4f, 0.6f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+        {0.3f, 0.6f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+
+        {0.55f, 0.7f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+        {0.3f, 0.6f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},  //옥상 테두리
+
+
+        {-0.4f, 0.6f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+        {-0.4f, -0.9f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+
+        {-0.4f, -0.9f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+        {0.3f, -0.9, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+
+        {0.3f, 0.6f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+        {0.3f, -0.9f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+
+        {-0.4f, 0.6f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+        {0.3f, 0.6f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},  //건물 앞면 테두리
+
+
+        {0.55f, -0.65f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+        {0.55f, 0.7, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+
+        {0.55f, -0.65f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},
+        {0.3f, -0.9f, 0.0f, D3DXCOLOR(0.153f, 0.153f, 0.204f, 1.0f)},  //건물 옆면 테두리
+    };
+
+
+    // create the vertex buffer
+    //버텍스 버퍼를 셋업하는 문구
+    D3D11_BUFFER_DESC bd;
+    ZeroMemory(&bd, sizeof(bd));
+
+    bd.Usage = D3D11_USAGE_DYNAMIC;                // write access access by CPU and GPU
+    bd.ByteWidth = sizeof(VERTEX) * 26;             // size is the VERTEX struct * 4
+    bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;       // use as a vertex buffer
+    bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;    // allow CPU to write in buffer
+
+    dev->CreateBuffer(&bd, NULL, &pVBuffer2);       // create the buffer  
+    ////여기까지가 셋업 문구. 복사 붙여넣기 해서 다른 버퍼도 추가. 대신 뒤의 이름만 바꿔주기./////////////////
+
+
+    // copy the vertices into the buffer
+    D3D11_MAPPED_SUBRESOURCE ms;
+    devcon->Map(pVBuffer2, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &ms);    // map the buffer
+    memcpy(ms.pData, OurVertices, sizeof(OurVertices));                 // copy the data
+    devcon->Unmap(pVBuffer2, NULL);                                      // unmap the buffer
+}
+
+
+// this function loads and prepares the shaders
+void InitPipeline()
+{
+    // load and compile the two shaders
+    ID3D10Blob* VS, * PS;
+    D3DX11CompileFromFile(L"shaders.shader", 0, 0, "VShader", "vs_4_0", 0, 0, 0, &VS, 0, 0);
+    D3DX11CompileFromFile(L"shaders.shader", 0, 0, "PShader", "ps_4_0", 0, 0, 0, &PS, 0, 0);
+
+    // encapsulate both shaders into shader objects
+    dev->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &pVS);
+    dev->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &pPS);
+
+    // set the shader objects
+    devcon->VSSetShader(pVS, 0, 0);
+    devcon->PSSetShader(pPS, 0, 0);
+
+    // create the input layout object
+    D3D11_INPUT_ELEMENT_DESC ied[] =
+    {
+        {"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+        {"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0},
+    };
+
+    dev->CreateInputLayout(ied, 2, VS->GetBufferPointer(), VS->GetBufferSize(), &pLayout);
+    devcon->IASetInputLayout(pLayout);
+}
